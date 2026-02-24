df9d09c942657c70c9999c54a9f1f77e3637d879
Ankit Sonkar <sonkarankit624@gmail.com>
2026-02-24 23:56:45 +0530

    Add files via upload

---
+++ Functions.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Functions {
+    
+    public static void PrintMyName ( String name ){
+        System.out.println("  Your Input : " + name);
+        return;
+    }
+    public static void main ( String args [] ){
+        Scanner sc = new Scanner ( System.in );
+        String name = sc.next();
+        PrintMyName(name);
+    }
+}
